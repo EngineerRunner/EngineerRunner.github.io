2dd9e73f53a122685639b8be3e394fa8ca081e40
EngineerRunner <123766662+EngineerRunner@users.noreply.github.com>
2023-04-04 19:04:18 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,1 @@
+ext._shutdown = function() {};ext._getStatus = function() { return {status: 2, msg: 'Ready'};}; ext.myBlock = function (callback){ window.alert('Hello!'); }; var descriptor = { blocks: [ [" ","( hello)","myBlock"],],menus: {},url: 'https://kyleplo.github.io/scratch-extension-creator/help#'Hello!};ScratchExtensions.register('My Extension', descriptor, ext);
